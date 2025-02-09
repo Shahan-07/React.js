58edfc9896e09714bd43b2d845a920499a60c07a
Shahan-07 <iamshahanalam22@gmail.com>
2025-02-09 13:31:19 +0500

    vite first project understanding

--- 01vitereactproject/src/App.jsx
+++ 01vitereactproject/src/App.jsx
@@ -6,6 +6,9 @@ function App() {
     <div>
       <h1>Hello this is react first step</h1>
     </div>
+
+    {/* in vite we have to name the file extension to jsx and also we have to look at the
+    naming convention, we have to look at thier first letter to be capatilize */}
     </>
   )
 }
