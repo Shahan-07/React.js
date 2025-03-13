6235e2958759b5296d69bf80f5db3f31c9ff4304
Shahan-07 <iamshahanalam22@gmail.com>
2025-03-13 19:01:50 +0500

    Redux Toolkit

--- reduxTooltikTodo/src/App.jsx
+++ reduxTooltikTodo/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import AddTodo from './componenets/AddTodo'
+import Todos from './componenets/Todo'
 
 function App() {
 
   return (
     <>
       <h1>Learn about ReduxToolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
