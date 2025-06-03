b99460831c66e9e56b901877f05337810656ffe7
Shahan-07 <iamshahanalam22@gmail.com>
2025-06-03 15:22:43 +0500

    React-Redux-Todo-project

--- reduxtoolkitpractice/src/components/Todos.jsx
+++ reduxtoolkitpractice/src/components/Todos.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import { useSelector,useDispatch } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
-
-
 function Todos() {
-    const todos = useSelector(state => state.todos)
+    const todos = useSelector(state =>state.todos)
     const dispatch = useDispatch()
 
     return (
